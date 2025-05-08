19bd332cab6f97a23b8ef78191ae6bcfb72c3663
Ekpere05 <ekpereamakamarjos2024@gmail.com>
2025-05-08 16:50:14 +0100

    final-repo

--- index.js
+++ index.js
@@ -6,16 +6,58 @@ function searchPage() {
         "foundation": "foundation.html",
     };
 
-    // Directly check if the search term matches any key in pages (case insensitive)
+
     if (pages[searchPage]) {
         window.location.href = pages[searchPage]; // Redirect to the appropriate page
     } else {
         alert("Page not found! Try searching for 'foundation', 'lipstick', or 'powder'.");
     }
-}
+    document.getElementById("paymentForm").addEventListener("submit", function(event) {
+        event.preventDefault();
 
-document.getElementById("search-input").addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        searchPage(); // Execute search when Enter is pressed
+        document.getElementById("paymentForm").classList.add("hidden");
+        document.getElementById("confirmation").classList.remove("hidden");
     }
+)
+// }
+
+// document.getElementById("search-input").addEventListener("keypress", function(event) {
+    // if (event.key === "Enter") {
+        // searchPage(); // Execute search when Enter is pressed
+    }
+// });
+// function placeOrder() {
+    // alert("Your order has been placed!");
+// }
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("paymentForm");
+    const confirmation = document.getElementById("confirmation");
+    const uploadSection = document.getElementById("uploadSection");
+    const receiptUpload = document.getElementById("receiptUpload");
+    const submitReceipt = document.getElementById("submitReceipt");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        form.classList.add("hidden");
+        confirmation.classList.remove("hidden");
+        uploadSection.classList.remove("hidden"); 
+    });
+
+    submitReceipt.addEventListener("click", function () {
+        if (receiptUpload.files.length === 0) {
+            alert("Please upload a receipt before submitting.");
+         } else {
+            alert("Receipt upload successfully!");
+         }
+    });
 });
+
+    // const confirmation = document.getElementById("confirmationMessage");
+
+    // if(name === "" || email === "" || phone === "" || quantity === "") {
+        // alert("Please fill all the fields!");
+    // } else {
+        // confirmation.innerHTML = "Thank you!" + name + "! Your order for" + quantity + "" + product + "(s) has been successfully placed. We will contact you shortly!";
+    // }
+
